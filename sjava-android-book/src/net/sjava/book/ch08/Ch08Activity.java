e1c1ed1447b442c82a26dacee0c4c716a42f436d
Justin(Moo Chan) Song <mcsong@gmail.com>
2015-05-15 21:34:22 -0700

    AsyncTask 타임아웃 추가

--- sjava-android-book/src/net/sjava/book/ch08/Ch08Activity.java
+++ sjava-android-book/src/net/sjava/book/ch08/Ch08Activity.java
@@ -1,17 +1,22 @@
 package net.sjava.book.ch08;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Process;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-
 import net.sjava.book.AbstractActivity;
 import net.sjava.book.R;
+
 
 public class Ch08Activity extends AbstractActivity {
 	
@@ -20,6 +25,8 @@ public class Ch08Activity extends AbstractActivity {
 	private Button mBtn03;
 	private Button mBtn04;
 	private Button mBtn05;
+	private Button mBtn06;
+	private Button mBtn07;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,8 @@ public class Ch08Activity extends AbstractActivity {
 		mBtn03 = (Button) findViewById(R.id.activity_ch08_btn_03);
 		mBtn04 = (Button) findViewById(R.id.activity_ch08_btn_04);
 		mBtn05 = (Button) findViewById(R.id.activity_ch08_btn_05);
+		mBtn06 = (Button) findViewById(R.id.activity_ch08_btn_06);
+		mBtn07 = (Button) findViewById(R.id.activity_ch08_btn_07);
 		
 		mBtn01.setOnClickListener(new OnClickListener() {
 			@Override
@@ -93,6 +102,37 @@ public class Ch08Activity extends AbstractActivity {
 				Log.d(CNAME, PriorityThreadBExampleTask.THREAD_POOL_EXECUTOR.toString() );
 			}
 		});
+		
+		mBtn06.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ProgressDialog pd = new ProgressDialog(v.getContext());
+				pd.setTitle("");		
+				pd.setMessage("Loading...");
+				pd.setCancelable(true);
+				
+				LoadingTask task = new LoadingTask(pd);
+				// 1초를 주기로 10초 동안 스레드 실행을 허용
+				new AsyncTaskCancelTimer(task, 10000, 1000, true).start();
+				task.execute("");
+			}
+		});
+		
+		mBtn07.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ProgressDialog pd = new ProgressDialog(v.getContext());
+				pd.setTitle("");		
+				pd.setMessage("Loading...");
+				pd.setCancelable(true);
+				
+				LoadingTask task = new LoadingTask(pd);
+				// 1초를 주기로 10초 동안 스레드 실행을 허용
+				new AsyncTaskCancelTimer(task, 10000, 1000, true).start();
+				task.execute("");
+			}
+		});		
+		
 	}
 	
 
@@ -216,4 +256,104 @@ public class Ch08Activity extends AbstractActivity {
 			return null;
 		}
 	}
+	
+	
+	@SuppressWarnings("rawtypes")
+	static class AsyncTaskCancelTimer extends CountDownTimer {
+		private AsyncTask asyncTask;
+		private boolean interrupt;
+		
+		private AsyncTaskCancelTimer(AsyncTask asyncTask, long startTime, long interval) {
+			super(startTime, interval);
+			this.asyncTask = asyncTask;
+		}
+				
+		private AsyncTaskCancelTimer(AsyncTask asyncTask, long startTime, long interval, boolean interrupt) {
+			super(startTime, interval);
+			this.asyncTask = asyncTask;
+			this.interrupt = interrupt;
+		}
+		
+		@Override
+		public void onTick(long millisUntilFinished) {
+			Log.d("AsyncTaskCancelTimer", "onTicked at " + new SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(new Date()) );
+			
+			if(asyncTask == null) {
+				this.cancel();
+				return;
+			}
+						
+			if(asyncTask.isCancelled())
+				this.cancel();
+			
+			if(asyncTask.getStatus() == AsyncTask.Status.FINISHED)
+			    this.cancel();
+		}
+
+		@Override
+		public void onFinish() {
+			Log.d("AsyncTaskCancelTimer", "onTick..");
+			
+			if(asyncTask == null || asyncTask.isCancelled() )
+				return;
+			
+			try {
+				if(asyncTask.getStatus() == AsyncTask.Status.FINISHED)
+					return;
+				
+				if(asyncTask.getStatus() == AsyncTask.Status.PENDING || 
+					asyncTask.getStatus() == AsyncTask.Status.RUNNING ) {
+					
+					asyncTask.cancel(interrupt);
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	static class LoadingTask extends AsyncTask<String, Integer, Boolean> {
+		private ProgressDialog pd = null;
+				
+		public LoadingTask(ProgressDialog pd) {
+			this.pd = pd;
+		}
+		
+		@Override
+		protected void onPreExecute() {	
+			super.onPreExecute();
+			if(pd != null)
+				pd.show();
+			
+			Log.d("LoadingTask", "onPreExecute..");
+		}
+		
+		@Override
+		protected Boolean doInBackground(String... params) {			
+			try {
+				Thread.sleep(1000 * 20);
+			} catch(InterruptedException e) {
+				Log.d("LoadingTask", "Exception : " + e.getLocalizedMessage());
+			}
+			
+			return Boolean.TRUE;
+		}
+
+		@Override
+		protected void onCancelled(Boolean result) {
+			Log.d("LoadingTask", "onCancelled : " + result);
+			
+			if(pd != null)
+				pd.dismiss();
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			Log.d("LoadingTask", "onPostExecute : " + result);
+			
+			if(pd != null)
+				pd.dismiss();
+		}
+	}
+	
 }
